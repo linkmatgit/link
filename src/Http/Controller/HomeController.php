1091aff1b7e23a72e49f82c609baa389d235cd27
linkmat <mathieugendron86@icloud.com>
2021-03-28 01:05:59 -0400

    Update to PHP 8 fix #3

---
+++ src/Http/Controller/HomeController.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Http\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class HomeController extends AbstractController
+{
+
+    /**
+     * @Route("/", name="app_home")
+     */
+    public function HomeFunction(): Response
+    {
+        return $this->render('pages/index.html.twig');
+    }
+}
